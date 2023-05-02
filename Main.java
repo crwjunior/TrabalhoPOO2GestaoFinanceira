79adc89a9eb25a1a4217366e43359d5293938b99
Carlos R. W. Júnior <106553307+crwjunior@users.noreply.github.com>
2023-05-02 19:40:56 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,16 +5,15 @@ import java.util.List;
 class Main {
   public static void main(String[] args) {
     List<Gastos> gastos = new ArrayList<>();
-    gastos.add(new Gastos("Celesc", new Date(), 140.0, "Cartão de Crédito"));
-    gastos.add(new Gastos("Cinema", new Date(), 60.0, "PIX"));
+    gastos.add(new Gastos("Cinema", new Date(), 60.0, "Cartão de Débito"));
     gastos.add(new Gastos("Mercado", new Date(), 600.0, "Dinheiro"));
-    gastos.add(new Gastos("Gasolina", new Date(), 200.0, "Cartão de Débito"));
-    gastos.add(new Gastos("Roupas", new Date(), 450.0, "PIX"));
-    gastos.add(new Gastos("Presentes", new Date(), 400.0, "Cartão de Crédito"));
+    gastos.add(new Gastos("Tênis", new Date(), 400.0, "Cartão de Débito"));
+    gastos.add(new Gastos("Hambúrguer", new Date(), 50.0, "PIX"));
+    gastos.add(new Gastos("Viagem", new Date(), 1250.0, "Cartão de Crédito"));
 
     List<Ganhos> ganhos = new ArrayList<>();
-    ganhos.add(new Ganhos("Salário", new Date(), 2500.0));
-    ganhos.add(new Ganhos("Freelance", new Date(), 1300.0));
+    ganhos.add(new Ganhos("Salário", new Date(), 6900.0));
+    ganhos.add(new Ganhos("Apostas(.bet)", new Date(), 1313.0));
 
     Relatorios relatorio = new Relatorios();
     relatorio.gerarRelatorioGastos(gastos);
